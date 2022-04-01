53a8c58d53177323e22c77076dcc2414e9c0ee41
pipysk1 <49283427+pipysk1@users.noreply.github.com>
2022-04-01 09:06:21 +0700

    Revert "Merge pull request #11 from pipysk1/mai_code"
    update addProduct  API

--- js/tables/tables.js
+++ js/tables/tables.js
@@ -1,28 +1,26 @@
-
-
 var token = localStorage.getItem('token');
 console.log(token);
 var fasionApi = 'https://hieuhmph12287-lab5.herokuapp.com/products/getProducts?token=' + token;
+var addProductApi = 'https://hieuhmph12287-lab5.herokuapp.com/products/addProduct?token=' + token;
+
 function start() {
     getAllProducts(renderProducts);
+    handleCreateForm();
 }
 start();
 
 async function getAllProducts(callback) {
     await fetch(fasionApi)
-        .then(function (response) {
+        .then(function(response) {
             return response.json();
         }).then(callback);
 }
 
 function renderProducts(products) {
-    var listProducts = document.querySelector('#list-products');
-    var tableHead = document.querySelector('thead');
-    var tBodyHead = document.querySelector('tbody');
     var tbody = document.querySelector('#data');
     var seq = 0;
 
-    var htmls = products.map(function (product) {
+    var htmls = products.map(function(product) {
         return `
         <tr>
         <th>${seq += 1}</th>
@@ -44,3 +42,64 @@ function renderProducts(products) {
     tbody.innerHTML = htmls.join('');
     // tableHead.innerHTML = htmls.join('');
 }
+
+function handleCreateForm() {
+
+    var createBtn = document.querySelector('#create');
+    createBtn.onclick = function(e) {
+        e.preventDefault();
+        var name_products = document.querySelector('input[name="name_products"]').value;
+        var status = document.querySelector('input[name="optionsRadios"]').value;
+        var gender = document.querySelector('input[name="optionsRadios1"]').value;
+        var type = document.getElementById('list-type').value;
+        var file = document.getElementById('imgFile');
+        var collection_id = document.querySelector('input[name="collection_id"]').value;
+        var price = document.querySelector('input[name="numbernew"]').value;
+        var old_price = document.querySelector('input[name="numberold"]').value;
+        var product_detail = document.getElementById('product_detail').value;
+        var formData = new FormData();
+        formData.append("name", name_products);
+        formData.append("gender", gender);
+        formData.append("price", price);
+        formData.append("old_price", old_price);
+        formData.append("product_detail", product_detail);
+        formData.append("status", status);
+        formData.append("type", type);
+        formData.append("file", fileInput.files[0], file);
+        var requestOptions = {
+            method: 'POST',
+            body: formData,
+            redirect: 'follow'
+        };
+        fetch("https://hieuhmph12287-lab5.herokuapp.com/products/addProduct?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjIzYWQyMTMxMzhiNjUwMDIzNmMwZGM1IiwidXNlcm5hbWUiOiJhZG1pbiIsImlhdCI6MTY0ODcyNDE3MiwiZXhwIjoxNjQ4ODEwNTcyfQ.8LnZ5rGBqOQtXbXmDgta6WbsuwGblflnqonUShj-2Ew", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+}
+
+
+
+
+
+// var formdata = new FormData();
+// formdata.append("file", fileInput.files[0], "/C:/Users/LvMai/Documents/Zalo Received Files/z3256746378248_8bbf5ed66540584653079a173bda552f.jpg");
+// formdata.append("name", "ao khoac");
+// formdata.append("gender", "Male");
+// formdata.append("price", "10000");
+// formdata.append("old_price", "200000");
+// formdata.append("product_detail", "asssadas");
+// formdata.append("status", "Dang xu ly123");
+// formdata.append("type", "Áo khoác");
+
+// var requestOptions = {
+//     method: 'POST',
+//     body: formdata,
+//     redirect: 'follow'
+// };
+
+// fetch("https://hieuhmph12287-lab5.herokuapp.com/products/addProduct?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjIzYWQyMTMxMzhiNjUwMDIzNmMwZGM1IiwidXNlcm5hbWUiOiJhZG1pbiIsImlhdCI6MTY0ODcyNDE3MiwiZXhwIjoxNjQ4ODEwNTcyfQ.8LnZ5rGBqOQtXbXmDgta6WbsuwGblflnqonUShj-2Ew", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+// }
