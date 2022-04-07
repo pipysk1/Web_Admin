ee4041cb2c1bc3c77f68537f4852534862f873b8
VuCV <vucv@newit.co.jp>
2022-04-07 15:06:03 +0700

    Admin | update panigation

--- api/detailBill.js
+++ api/detailBill.js
@@ -4,8 +4,6 @@ var id_bill = localStorage.getItem('id_bill')
 // console.log(token);
 var url = 'https://hieuhmph12287-lab5.herokuapp.com/'
 var fasionApi = 'https://hieuhmph12287-lab5.herokuapp.com/products/getProducts?token=' + token;
-var addProductApi = 'https://hieuhmph12287-lab5.herokuapp.com/products/addProduct?token=' + token;
-
 getAllProducts(renderProducts);
 
 
@@ -13,12 +11,12 @@ getAllProducts(renderProducts);
 async function getAllProducts(callback) {
     displayLoading();
     await fetch(url + 'bills/getBills' + '?token=' + token)
-    
-        .then(function(response) {
+
+        .then(function (response) {
             return response.json();
-       
+
         }).then(callback);
-        hideLoading();
+    hideLoading();
 }
 
 function renderProducts(products) {
@@ -27,13 +25,14 @@ function renderProducts(products) {
     console.log(data)
     for (let i = 0; i < data.length; i++) {
         if (data[i].bill_id == id_bill) {
-       
-            var bill_id = document.getElementById('bill_id').value = data[i].bill_id;
-            var name_products = document.getElementById('name').value = data[i].product[0].name;
+
+            var bill_id = document.getElementById('bill_id').value = data[i].bill_id.slice(0, 6);
+            var name_products = document.getElementById('name').value = data[i].name_receiver;
             var phone_number = document.getElementById('phone').value = data[i].phone_number;
             var status = document.getElementById('status').value = data[i].status;
             var discount_value = document.getElementById('discount').value = data[i].discount_value;
             var payment_type = document.getElementById('payment').value = data[i].payment_type;
+            var address = document.getElementById('address').value = data[i].address_detail;
             var price = document.getElementById('price').value = data[i].product[0].price;
         }
     }
